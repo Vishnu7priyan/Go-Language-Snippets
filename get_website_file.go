aba2bbdb45f2c8221b2438a245ee1a4e3584c582
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-09-12 20:35:25 +0530

    Update get_website_file.go

--- get_website_file.go
+++ get_website_file.go
@@ -11,7 +11,10 @@ r , err := http.Get("http://www.example.com/index.html")
 if err != nil{
         fmt.Println("Error")
         }
+	defer response.Body.Close()
+
 bodyLen := 1300 //Change the length based on your requirement
 webOut := make([]byte,bodyLen)
 r.Body.Read(webOut)
+        
 }
