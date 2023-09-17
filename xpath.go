34dce49dc075df59e483f235474b97f390d0ce1d
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-09-17 16:32:31 +0530

    Update xpath.go

--- xpath.go
+++ xpath.go
@@ -13,5 +13,10 @@ println(doc)
  if err != nil {
  panic(err)
  }
- 
+ println("Result !")
+ println("----------------------------------")
+ for _, node := range TextNodes {
+ text := strings.TrimSpace(node.Data)
+ println(text)
+ }
 }
