3f33776dc338780af4cff35a31971828895f5bec
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-09-18 15:34:13 +0530

    Update css_selector.go

--- css_selector.go
+++ css_selector.go
@@ -1,1 +1,15 @@
 
+package main
+import (  "fmt"  "strconv"  "github.com/PuerkitoBio/goquery")
+func main() {  
+  doc, err := goquery.NewDocument("URL")
+  if err != nil {   
+    panic(err)  }  
+  doc.Find(`css selector here `).   
+  Each(func(i int, e *goquery.Selection) {     
+    a,_ = e.Attr("change here")     
+    b, _ := e.Attr("change here")     
+    c, err = strconv.ParseFloat(b, 64)     
+    if err != nil {        
+      println("Failed to parse ")      }    
+    fmt.Printf("%s ($%0.2f)\n", a, c)    })}
