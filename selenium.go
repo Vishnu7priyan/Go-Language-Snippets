6608fcd8664912b0d5f5c9b2f7aab062ea011919
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-09-24 20:56:05 +0530

    Update selenium.go

--- selenium.go
+++ selenium.go
@@ -5,4 +5,8 @@ service, err := selenium.NewSeleniumService(    seleniumPath,     8080,     sele
 if err != nil {    
   panic(err)  } 
 defer service.Stop() 
+caps := selenium.Capabilities{"browserName": "firefox"}  
+wd, err := selenium.NewRemote(caps, "http://localhost:8080/") 
+if err != nil {    panic(err)  } 
+defer wd.Quit()
 }
