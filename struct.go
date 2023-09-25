858afd5f4df55f8bc1a2534f60bd7a209d127a5c
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-09-25 17:02:12 +0530

    Update struct.go

--- struct.go
+++ struct.go
@@ -6,3 +6,10 @@ type student struct{
 rollno int
 name string
 }
+func main(){
+ var s1 Student = Student{101,"vishnu"}
+ var s2 Student  = Student{name:"priyan"}
+ fmt.Println(s1)
+ fmt.Println(s1.name)
+ fmt.Println(s2)
+}
