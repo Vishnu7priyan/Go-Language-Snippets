1abbff428376223b0a49dfc31b418107f0d5e4fd
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-09-30 18:18:17 +0530

    Update map.go

--- map.go
+++ map.go
@@ -6,7 +6,9 @@ import(
 func main(){
   colors := map[string] string{
     "Blue":"#f0f8ff",
+    "Coral":"#ff7f50",
   }
   for key,value := range colors{
     fmt.Println("%s and %s " , key,value}
 }
+delete(colors,"Blue")
