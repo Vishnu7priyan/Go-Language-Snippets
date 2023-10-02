d36194dd0d647a3ad0d6e5195fc7cf89c5696d43
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-10-02 18:48:16 +0530

    Update flag.go

--- flag.go
+++ flag.go
@@ -9,5 +9,10 @@ func main() {
     var yourname string
     var age int
 
+    flag.StringVar(&yourname, "name", "", "Your name")
+    flag.IntVar(&age, "age", 0, "Your age")
 
+    flag.Parse()
+
+    
 }
