a211b376a5f0a6910b8d6de3919de8e21f0fdb26
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-10-03 17:41:39 +0530

    Update Arrays.go

--- Arrays.go
+++ Arrays.go
@@ -2,3 +2,6 @@
 var array [5]int
 //Declare with values 
 array := [5]int{10, 20, 30, 40, 50}
+//Capacity based on no. of values
+array := [...]int{10, 20, 30, 40, 50}
+//Declaring on specific index values
