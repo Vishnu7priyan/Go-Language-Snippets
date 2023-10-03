5cf7afccd8e580320eb41dcfedf1fb1fa2355dc9
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-10-03 17:41:49 +0530

    Update Arrays.go

--- Arrays.go
+++ Arrays.go
@@ -5,3 +5,6 @@ array := [5]int{10, 20, 30, 40, 50}
 //Capacity based on no. of values
 array := [...]int{10, 20, 30, 40, 50}
 //Declaring on specific index values
+array := [5]int{1: 10, 2: 20}
+//Change The value
+array[2] = 35;
