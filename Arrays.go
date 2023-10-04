f76a92c4a8203fc7b3f977b1d125feb7b06f8f70
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-10-04 20:13:01 +0530

    Update Arrays.go

--- Arrays.go
+++ Arrays.go
@@ -8,3 +8,6 @@ array := [...]int{10, 20, 30, 40, 50}
 array := [5]int{1: 10, 2: 20}
 //Change The value
 array[2] = 35;
+var array1 [5]string 
+array2 := [5]string{"Red", "Blue", "Green", "Yellow", "Pink"}
+array1 = array2
