efa9214d8de845d08c2335239469cb7fe018c896
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-10-06 18:00:58 +0530

    Update Time.go

--- Time.go
+++ Time.go
@@ -7,4 +7,8 @@ import (
 func main() {
     currentTime := time.Now()
     fmt.Println("Current Time is :", currentTime)
+    Tomm := currentTime.Add(24 * time.Hour)
+    pastTime := currentTime.Add(-1 * time.Hour)
+    fmt.Println("Future Time:", Tomm)
+    fmt.Println("Past Time:", pastTime)
 }
