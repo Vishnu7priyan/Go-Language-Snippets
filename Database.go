13c4407bf2cd5da72fd3b0d4008418baeaa6d23a
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-10-07 19:52:44 +0530

    Update Database.go

--- Database.go
+++ Database.go
@@ -1,1 +1,16 @@
 
+package main
+
+import (
+    "database/sql"
+    "fmt"
+    "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+    db, err := sql.Open("sqlite3", "./example.db")
+    if err != nil {
+        fmt.Println("Error :", err)
+    }
+    defer db.Close()
+}
