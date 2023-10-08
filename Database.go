c52aaa874f0e520889ca6bc79fc70e744c47268a
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-10-08 20:25:21 +0530

    Update Database.go

--- Database.go
+++ Database.go
@@ -5,6 +5,7 @@ import (
     "database/sql"
     "fmt"
     "github.com/mattn/go-sqlite3"
+    //Install with go get <URL>
 )
 
 func main() {
@@ -12,6 +13,7 @@ func main() {
     if err != nil {
         fmt.Println("Error :", err)
     }
+    //Table creation
     createTable := `
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,13 +25,15 @@ func main() {
     if err != nil {
         fmt.Println("Error :", err)
     }
-    
+
+    //Updation
     updateSQL := "UPDATE users SET email = ? WHERE username = ?"
     _, err = db.Exec(updateSQL, "test@example.com", "vishnu")
     if err != nil {
         fmt.Println("Error updating data:", err)
         return
     }
+    //Deletion
     deleteSQL := "DELETE FROM users WHERE username = ?"
     _, err = db.Exec(deleteSQL, "vishnu")
     if err != nil {
