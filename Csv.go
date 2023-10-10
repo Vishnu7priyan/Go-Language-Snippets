da0968b41ae553a8c5b334f45579a3fecaba9d28
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-10-10 19:24:31 +0530

    Update Csv.go

--- Csv.go
+++ Csv.go
@@ -5,3 +5,26 @@ import (
     "os"
 )
 
+func main() {
+    // Create a CSV file for writing
+    file, err := os.Create("data.csv")
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+   writer := csv.NewWriter(file)
+    defer writer.Flush()
+
+    data := [][]string{
+        {"Name", "Age", "City"},
+        {"vishnu", "19", "Bash"},
+    }
+    for _, row := range data {
+        if err := writer.Write(row); 
+        err != nil {
+            panic(err)
+        }
+    }
+    
+}
+
