6675e756ee69eb168b4073f8354f53c9a93cb137
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-10-11 20:11:27 +0530

    Update Mongodb.go

--- Mongodb.go
+++ Mongodb.go
@@ -7,3 +7,19 @@ import (
     "go.mongodb.org/mongo-driver/mongo"
     "go.mongodb.org/mongo-driver/mongo/options"
 )
+
+
+func main() {
+    client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    db := client.Database("mydb")
+    collection := db.Collection("mycollection")
+
+    document := bson.D{{"name", "John Doe"}, {"age", 30}}
+
+    
+}
