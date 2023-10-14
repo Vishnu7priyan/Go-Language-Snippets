af47484f4ff30a0a352e78cf457c1237a5fea400
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-10-14 20:59:46 +0530

    Update xpath+html-tool.go

--- xpath+html-tool.go
+++ xpath+html-tool.go
@@ -1,1 +1,21 @@
+//html-tool is developed by tomnomnom 
 
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "regexp"
+    "os/exec"
+    execute "github.com/alexellis/go-execute/pkg/v1"
+)
+
+func GetAllUrls(user_input string) {
+    url := "https://"+user_input
+    resp, err := http.Get(url)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close() // Close the response body to avoid resource leaks.
+
+   
+}
