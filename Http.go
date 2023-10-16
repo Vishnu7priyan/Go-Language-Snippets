31530971c6bba6b1c74140b19ad670be809cb36f
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-10-16 17:20:07 +0530

    Update Http.go

--- Http.go
+++ Http.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+    "fmt"
+    "net/http"
+    ")
+
+func main(){
+  url := "URL_HERE"
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+	fmt.Println("GET Response:")
+	fmt.Println(string(body))
+
+  
+  }
