649592c8401d763da19681534fa20d66ebfbd5df
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-10-17 21:01:43 +0530

    Update TextFile.go

--- TextFile.go
+++ TextFile.go
@@ -1,1 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+    filePath := "textfile.txt"
+    file, err := os.Open(filePath)
+    if err != nil {
+        fmt.Println("Error opening the file:", err)
+        return
+    }
+    defer file.Close() 
+
+}
 
